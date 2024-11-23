04cef51226863e3a7ca14cba2d2e18dc87621a7b
abhi <abhi@gmail.com>
2024-11-24 01:52:23 +0530

    cqueue

--- C programming/cque.c
+++ C programming/cque.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <conio.h>
-    int size, arr[10], item, i, rear = -1, front = -1, op;
+    int size, arr[100], i, rear = -1, front = -1;
     int enque(int arr[], int size);
     int deque(int arr[], int size);
+    int peek(int arr[]);
     void main()
     {
-
-      printf("Enter the size of array:");
+        int op;
+      printf("Enter the size of circular queue:");
       scanf("%d", &size);
       while (1)
       {
-        printf("\n\nwhich operation you want to perform 1.enque \n 2. deque \n 3. display \n ");
+        printf("\n\nwhich operation you want to perform 1.enque \n 2. deque \n 3. peek \n 4. display \n ");
         scanf("%d", &op);
         switch (op)
         {
@@ -26,6 +27,12 @@
         }
         case 3:
         {
+            peek(arr);
+            break;
+        }
+        
+        case 4:
+        {
           if (front == -1)
           {
             printf("Queue is empty");
@@ -39,67 +46,40 @@
         }
       }
     }
-    int enque(int arr[], int size)
-    {
 
-      if (rear == size - 1 || front - rear == -1)
-        printf("stack is full");
-
-            else if (front == -1 && rear == -1)
-        {
-          front = 0;
-          rear = 0;
-          for (i = 0; i < size; i++)
-          {
-            printf("enter the item to be inserted ");
-            scanf("%d", &item);
-            arr[rear] = item;
-          }
+    int enque(int arr[], int size){
+        int item;
+        printf("Enter the element to be inserted: ");
+        scanf("%d",&item);
+        if((rear+1)%size==front){
+            printf("The queue is full!!!");
         }
-      else
-      {
-        rear = (rear + 1) % size;
-        for (i = 0; i < size; i++)
-        {
-          printf("enter the item to be inserted ");
-          scanf("%d", &item);
-          arr[rear] = item;
+        else if(front==-1&&rear==-1){
+            front=0;
+            rear=0;
+            arr[rear]=item;
         }
-      }
-
-      int deque(int arr[], int size) {
-      {
-        printf("%d\n\n", front);
-        if (front == -1 || front > rear)
-        {
-          printf("Array is empty");
+        else{
+            rear=(rear+1)%size;
+            arr[rear]=item;
         }
-        else
-        {
-          printf("deleted item is %d", arr[front]);
-          front++;
-        }
-      }
-
-      else{
-      for (i = 0; i < size; i++)
-      {
-        printf("enter the item to be inserted ");
-        scanf("%d", &item);
-        C
-      }
     }
 
-  int deque(int arr[], int size)
-  {
-    printf("%d\n\n", front);
-    if (front == -1 || front > rear)
-    {
-      printf("Array is empty");
+    int deque(int arr[], int size){
+        if(front==-1){
+            printf("The queue is empty!!!");
+        }
+        else if(front==rear){
+            printf("deleted item is %d", arr[front]);
+            front=-1;
+            rear=-1;
+        }
+        else{
+            printf("deleted item is %d", arr[front]);
+            front=(front+1)%size;
+        }
     }
-    else
-    {
-      printf("deleted item is %d", arr[front]);
-      front++;
+
+    int peek(int arr[]){
+        printf("The item in front is %d",arr[front]);
     }
-  }
