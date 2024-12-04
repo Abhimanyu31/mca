fcd84fe656e7fe37bee02ba8219e7ab25f69ba72
AleenaElizabath <AleenaElizabathmanoj@gmail.com>
2024-12-04 15:52:23 +0530

    first commit

---
+++ login/register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student register</title>
+</head>
+<body>
+    <h1>Student Register</h1>
+    <form action="" method="$_POST">
+        <label for="name">Enter your name: </label><input type="text" name="name"> <br>
+        <label for="name">Enter your email address: </label><input type="text" name="email"> <br>
+        <label for="name">Enter your phone number: </label><input type="number" name="phno"> <br>
+        <label for="name">Enter your password: </label><input type="password" name="pword"> <br>
+        <label for="name">Please re-enter your password: </label><input type="password" name="name">
+        <input type="submit" name='Register'>
+    </form>
+</body>
+</html>
