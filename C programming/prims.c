f9f867f8c50e1b63b7ec2d82c14b3da2b217e965
Arundhati-Prabhald <adhilakhan02@gmail.com>
2024-12-19 15:11:45 +0530

    19-12-2024

--- C programming/prims.c
+++ C programming/prims.c
@@ -8,19 +8,7 @@ int mst[SIZE][SIZE];
 int max=0;
 
 int prims(int startnode){
-    int weight=max;
-    int key;
-    for(i=0;i<vertices;i++){
-        if(graph[startnode][i] != 0){
-            if(graph[startnode][i]<weight){
-                weight=graph[startnode][i]; 
-                key=i; 
-            }      
-        }
-        mst[startnode][i]=weight;
-        prims(i);
-    }
-    return mst;
+    printf("hi");
 }
 
 int main() {
@@ -38,26 +26,24 @@ int main() {
     // Assign edges to the graph
     int edge;
     for (i = 0; i < vertices; i++) {
-        for (j = 0; j < vertices; j++) { // Include diagonal (self-loop)
-            if (i == j) {
-                printf("\nDoes a self-loop exist at vertex %d? (if Yes, enter its weight; 0 for No): ", i);
-                scanf("%d", &edge);
-                if (edge!=0) {
-                    graph[i][i] = edge; // Self-loop at vertex i
-                }
-            } else {
-                printf("\nDoes an edge exist between vertex %d and vertex %d? (if Yes, enter its weight; 0 for No): ", i, j);
-                scanf("%d", &edge);
-                if (edge != 0) {
-                    graph[i][j] = edge;
-                    graph[j][i] = edge; // Undirected graph (symmetrical edge)
-                }
+    for (j = 0; j < vertices; j++) {
+        if (i < j) { // Only ask for the upper triangle of the matrix
+            printf("\nDoes an edge exist between vertex %d and vertex %d? (if Yes, enter its weight; 0 for No): ", i, j);
+            scanf("%d", &edge);
+            if (edge != 0) {
+                graph[i][j] = edge;
+                graph[j][i] = edge;  // Undirected graph: set both directions
             }
-            if(edge>max){
-                max=edge;
+        }
+        else if (i == j) { // Handle self-loops
+            printf("\nDoes a self-loop exist at vertex %d? (if Yes, enter its weight; 0 for No): ", i);
+            scanf("%d", &edge);
+            if (edge != 0) {
+                graph[i][i] = edge;  // Self-loop at vertex i
             }
         }
     }
+}
 
     // Printing the adjacency matrix
     printf("\nAdjacency Matrix:\n");
